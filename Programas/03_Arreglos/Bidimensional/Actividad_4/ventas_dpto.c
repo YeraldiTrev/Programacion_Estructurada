21f3bd0f37af9f1f67582660a9ebfe3f0f025905
Yeraldi Treviño <ytrevino1304@gmail.com>
2022-10-05 10:28:37 -0500

    Bug correction

--- Programas/03_Arreglos/Bidimensional/Actividad_4/ventas_dpto.c
+++ Programas/03_Arreglos/Bidimensional/Actividad_4/ventas_dpto.c
@@ -1,31 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define DPTOS 5
+#define MESES 12
 
 int main()
 {
     system("clear");
-    const int dptos=5,meses=12;
-    float anual=0,vtas_dptos[dptos][meses],vtas_mensuales[meses];
+    float anual=0,vtas_dptos[DPTOS][MESES],vtas_mensuales[MESES];
     int i,j,mayorDpto,mayorMes,menorMes;
     
-    for(i=0;i<meses;i++)
+    for(i=0;i<MESES;i++)
     {
         vtas_mensuales[i]=0;
     }
 
-    for(i=0;i<dptos;i++)
+    for(i=0;i<DPTOS;i++)
     {
         printf("Departamento %d\n",i+1);
-        for(j=0;j<meses;j++)
+        for(j=0;j<MESES;j++)
         {
             printf("\t%2d%c mes: ",j+1,167);
             scanf("%f",&vtas_dptos[i][j]);
         }
     }
 
-    for(i=0;i<meses;i++)
+    for(i=0;i<MESES;i++)
     {
-        for(j=0;j<dptos;j++)
+        for(j=0;j<DPTOS;j++)
         {
             vtas_mensuales[i]+=vtas_dptos[j][i];
             anual+=vtas_dptos[j][i];
@@ -35,14 +36,14 @@ int main()
     system("pause");
     system("clear");
 
-    for(i=0;i<meses;i++)
+    for(i=0;i<MESES;i++)
     {
         printf("Ventas del mes %2d: $%8.2f\n",i+1,vtas_mensuales[i]);
     }
     printf("Venta Anual: $%8.2f",anual);
 
     float mayorVta = vtas_dptos[0][5];
-    for(i=1;i<dptos;i++)
+    for(i=1;i<DPTOS;i++)
     {
         if(mayorVta<vtas_dptos[i][5])
         {
@@ -54,7 +55,7 @@ int main()
 
     mayorVta=vtas_dptos[2][0];
     float menorVta=vtas_dptos[2][0];
-    for(i=1;i<meses;i++)
+    for(i=1;i<MESES;i++)
     {
         if(mayorVta<vtas_dptos[2][i])
             mayorMes=i;
